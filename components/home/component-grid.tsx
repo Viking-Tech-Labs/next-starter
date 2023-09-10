9763b7ae3ea7394590356ed9f6e8a1dbb3354b94
Georgi Georgiev <auratiger00@gmail.com>
2023-09-10 23:20:22 +0300

    fix: prettier ignore

--- components/home/component-grid.tsx
+++ components/home/component-grid.tsx
@@ -3,8 +3,18 @@
 export default function ComponentGrid() {
   return (
     <div className="grid grid-cols-1 place-items-center gap-5 md:grid-cols-3">
-      <div className="border">Content</div>
-      <div className="border">Content</div>
+      <div
+        className="animate-fade-up opacity-0"
+        style={{ animationDelay: "0.35s", animationFillMode: "forwards" }}
+      >
+        Content
+      </div>
+      <div
+        className="animate-fade-up opacity-0"
+        style={{ animationDelay: "0.45s", animationFillMode: "forwards" }}
+      >
+        Content
+      </div>
     </div>
   );
 }
