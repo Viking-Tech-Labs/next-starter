fc268e417a1f4c40cf857d6a1fcc36dac4ce5812
Georgi Georgiev <auratiger00@gmail.com>
2023-09-10 12:53:56 +0300

    feat: cleaned up unneeded code

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,21 +2,14 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import useScroll from "@/lib/hooks/use-scroll";
 
 export default function NavBar() {
-  const scrolled = useScroll(50);
-
   return (
     <>
       <div
-        className={`fixed top-0 w-full flex justify-center ${
-          scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-            : "bg-white/0"
-        } z-30 transition-all`}
+        className={`fixed top-0 z-30 flex w-full justify-center transition-all`}
       >
-        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
+        <div className="mx-5 flex h-16 w-full max-w-screen-xl items-center justify-between">
           <Link href="/" className="flex items-center font-display text-2xl">
             <Image
               src="/logo.png"
@@ -25,7 +18,7 @@ export default function NavBar() {
               height="30"
               className="mr-2 rounded-sm"
             ></Image>
-            <p>Precedent</p>
+            <p>Viking Tech Labs</p>
           </Link>
         </div>
       </div>
